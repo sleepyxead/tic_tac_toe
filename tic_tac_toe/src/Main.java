9a466d8aa40f8ba6f4d9b5f2a163e0600bac7cff
sleepyxead <shi_foo@mail.ru>
2024-01-18 00:26:41 +0300

    -

---
+++ tic_tac_toe/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(field());
+
+    }
+
+    public static void field() {
+        String line1 = "|---|---|---|";
+        String line2 = "|-1-|-2-|-3-|";
+        String line3 = "|---|---|---|";
+        String line4 = "|-4-|-5-|-6-|";
+        String line5 = "|---|---|---|";
+        String line6 = "|-7-|-8-|-9-|";
+        String line7 = "|---|---|---|";
+    }
+}
